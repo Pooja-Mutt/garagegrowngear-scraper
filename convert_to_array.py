e6fd56d008a718983018a3aa48e8d7325e03d63d
Pooja Mutt <poojahm.ec@gmail.com>
2025-06-09 23:58:05 -0400

    Remove code and data files for portfolio, keep only README

--- convert_to_array.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("products.json", "r", encoding="utf-8") as infile:
-    data = json.load(infile)
-
-with open("products_array.json", "w", encoding="utf-8") as outfile:
-    json.dump(data, outfile, indent=2)
-
-print("Conversion complete! See products_array.json")
